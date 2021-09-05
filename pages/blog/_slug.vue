f59ccb50253cb4f5c473a7687261deae05cf4ee1
paopaoch <paopaochofficial@gmail.com>
2021-09-06 04:22:39 +0700

    add comment

--- pages/blog/_slug.vue
+++ pages/blog/_slug.vue
@@ -6,13 +6,14 @@
 <script>
 export default {
     async asyncData({ $content, params }) {
-        console.log(params)
-        const article = await $content('blog', params.slug).fetch();
-        console.log(article)
-        return {article}
+        console.log(params); // you can access the params like this 
+        // eg. url/blog/hello will return hello and url/blog/test will return test
+        // can use this params to fetch data from database
+
+        const article = await $content("blog", params.slug).fetch();
+        console.log(article);
+        return { article };
     }
-}
+};
 </script>
-<style lang="">
-    
-</style>
+<style lang=""></style>
